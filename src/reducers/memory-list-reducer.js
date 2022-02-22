ebfc42d61b78687754185f3d91adac300854bffd
Matt Luker <jamesmattluker@gmail.com>
2022-02-22 11:47:45 -0700

    add test 2 formemory-list-reducer.js and code to pass
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>

--- src/reducers/memory-list-reducer.js
+++ src/reducers/memory-list-reducer.js
@@ -1,3 +1,16 @@
 export default (state = {}, action) => {
-  return state;
+  const {title, description , date ,id} = action;
+  switch (action.type) {
+    case 'ADD_MEMORY':
+      return Object.assign({}, state, {
+        [id]: {
+          title: title,
+          description: description,
+          date:  date,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
