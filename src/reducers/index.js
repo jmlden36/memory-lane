7386eced38f6b16ebff2b9428e75689ca1d09fc4
Matt Luker <jamesmattluker@gmail.com>
2022-02-23 12:20:43 -0700

    WIP: trying to add firestor/firebase
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import formVisibleReducer from './form-visible-reducer';
 import memoryListReducer from './memory-list-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  mainMemoryList: memoryListReducer 
+  mainMemoryList: memoryListReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
