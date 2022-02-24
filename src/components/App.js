c00ce66d79a960f774d6e13d45dca8d6f5013188
Matt Luker <jamesmattluker@gmail.com>
2022-02-23 17:16:36 -0700

    add signin, signout, signup
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import MemoryControl from "./MemoryControl";
+import Header from "./Header";
+import Signin from "./Signin";
+import { BrowserRouter as Router, Switch , Route } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
-      <div className="container shadow">
-        <MemoryControl />
-      </div>
-    </React.Fragment>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+          <div className="container shadow">
+            <MemoryControl />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
